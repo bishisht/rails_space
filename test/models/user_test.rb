2f17d669deb6e22d3ca6bd29bc4d9cd4c0f6cc57
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2014-08-23 17:10:04 +0545

    adding tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,12 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should create user" do
+  	user=User.new
+  	user.screen_name = "bishisht2"
+  	user.email="bishisht@example.com"
+  	user.password="bandana"
+
+  	assert user.save
+  end
 end
